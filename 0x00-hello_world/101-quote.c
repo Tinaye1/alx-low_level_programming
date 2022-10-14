be726a6daa64f288eb431bebeb10a07244138224
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-13 21:14:06 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,10 +5,11 @@
  *
  * Return: 0 if program runs without error
  */
-
 int main(void)
 {
+
 fprintf(stderr, "and that piece of art is useful\"");
 fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
+
 return (1);
 }

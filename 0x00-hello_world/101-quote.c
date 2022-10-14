cd2d8827794189e7b3df6dcc01c59760610559e3
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-13 21:00:03 -0700

    c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /*
  * 
  * main - prints a quote by Dora Korpar to standard error
@@ -11,6 +10,5 @@ int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\"");
 	fprintf(stderr, " - Dora Korpar, 2015-10-19\n");
-
 	return (1);
 }

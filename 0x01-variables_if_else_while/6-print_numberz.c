815dc48958185f50d44740e1a4baa3a9cb8d24ec
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-14 21:20:42 -0700

    if

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main print base 10 numbers
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int num = 48;
+
+	while (num <= 57)
+	{
+		putchar(num);
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}

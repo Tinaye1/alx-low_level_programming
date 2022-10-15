14f4af90e5fd017f3a5db45240788b5fd0da4150
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-14 21:16:08 -0700

    if

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <time.h>
 
+/**
+ * main print base 10 numbers
+ *
+ * return:0 for success
+ */
 int main(void)
 {
 	char num = '0';
+
 	while (num <= '9')
 	{
 		putchar(num);

550caa0adc06892e6972907c9f7a5cb2b3021f2e
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-14 22:08:45 -0700

    if

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+*
+* main-prints base 16 numbers
+*
+* Return: 0
+* description- prints combination of 3 numbers
+*
+*/
+
+int main(void)
+{
+	int i, k, j;
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '0'; j <= '9'; j++)
+		{
+			for (k = '0'; k <= '9'; k++)
+			{
+				if ((i < j) & (j < k) & (k <='9'))
+				{
+					putchar(i);
+					putchar(k);
+					putchar(j);
+
+					if ((i + j + k) < 168)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+					

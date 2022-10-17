6eb3272fe2ba7be67c9c5667b44cf4a1bb2c0f92
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-17 13:05:06 -0700

    if

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main-prints base 16 numbers
+ *
+ * Return: 0
+ * description- prints hexadecimals
+ */
+int main(void){
+
+	char letter = 'a';
+	
+	while (letter <= 'z')
+	
+	{
+		putchar(letter);
+		letter++;
+	
+	}
+
+	letter = 'A';
+	
+	while (letter <= 'Z')
+	
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+	return (0);
+}

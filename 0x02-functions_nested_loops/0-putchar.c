66aed5c836f669429caa9b0704d6e6e97392b497
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-18 11:24:55 -0700

    loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main-prints putchar
+ * description:prints putchar and new line
+ *
+ * Return: 0 for success
+ */
+int main(void)
+{
+	printf("_putchar"+\n);
+
+return (0);
+}
+

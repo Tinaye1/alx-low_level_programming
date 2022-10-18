9235e27038cc34e1a6a89f3a19a0f46b7fbcced9
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-18 13:00:28 -0700

    loops

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *
+ * _islower returns 1 cahr is lowercase
+ *
+ * Return: 1 lowercase and otherwise
+ **/
+
+int _islower(int c){
+
+	if(c >= 97 && c <= 122)
+		return (1);
+	else
+		return(0);
+}

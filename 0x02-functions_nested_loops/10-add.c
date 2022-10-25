01385066216eff172f214422f0a989f12cf686ac
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-25 16:18:56 -0700

    loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
  * add-adds two numbers
- * @n1-The first integer
- * @n2-The second integer
+ * @n1: The first integer
+ * @n2: The second integer
  *
  * Return: result of the addition
  */

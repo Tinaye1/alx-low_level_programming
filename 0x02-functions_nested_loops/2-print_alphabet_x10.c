c86783a73855987e607f92181a351bcbfd97ccba
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-25 14:38:31 -0700

    loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,10 +2,21 @@
 #include "main.h"
 /**
  *
- * print_alphabet- prints lower case alphabets 10 times
+ * print_alphabet- prints lower case alphabet
  *
- * Return:0
  */
+void print_alphabet(void){
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+	_putchar(i);
+	}
+_putchar('\n);
+}
+/**
+ * print_alphabet_x10-prints alphaetts 10 times
+ *
+ **/
 void print_alphabet_x10(void)
 {
 

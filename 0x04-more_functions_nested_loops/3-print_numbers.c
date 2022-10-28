41e2e0135c3a8f8127d806f17a6b761ab536e87e
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-28 04:09:04 -0700

    loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,14 +3,13 @@
  * print_numbers - prints numbers between 0 and 9
  *
  * @i -int to be tested
- * Return: nothing
  **/
 void print_numbers(void)
 {
 
 int  i;
 
-for (i =0; i < 9; i++)
+for (i = 0; i < 9; i++)
 }
 	_putchar(i);
 	_putchar('\n');

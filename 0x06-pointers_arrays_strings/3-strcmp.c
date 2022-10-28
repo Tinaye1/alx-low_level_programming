a1cc383f3e4ec189ea7f30ee2f920fcd56ffb8dd
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-28 02:18:37 -0700

    pointers

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,20 +3,21 @@
 /**
  * _strcmp - function that compares two strings
  *
- * @n1:input one
+ * @s1:input one
  *
- *@n2:input two
+ *@s2:input two
  *
  *Return:0
  */
-
-int _strcmp(char *n1, char *n2)
+int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; n1[i] != '\0' && n2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		if (n1[i] != n2[i])
-			return (n1[i] - n2[i]);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
 	return (0);
+}
+

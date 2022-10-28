89348479a7a7f11db73a5e0353a917054d7aa6e2
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-28 03:59:43 -0700

    loops

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints numbers between 0 and 9
+ *
+ * @i -int to be tested
+ **/
+void print_numbers(void)
+{
+int i;
+
+for (i = 0; i < 9; i++)
+{
+	_putchar(i);
+	_putchar('\n');
+}
+}

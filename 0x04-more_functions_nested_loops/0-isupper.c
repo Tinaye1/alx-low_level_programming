9b84422a772439b89a162766eafa976ff2dbccda
Mitchelle Chabayanzara <Mitchelle.tchabayanzara@gmail.com>
2022-10-28 03:36:12 -0700

    loops

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * checks if a value is uppercase
+ * _isupper - checks if a value is uppercase
  * @c:character to be checked
  *
  * Return: 1 if c is capital, if not 0
- * */
+ */
 
 int _isupper(int c)
 {
